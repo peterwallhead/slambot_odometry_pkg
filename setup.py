e8a3851c6dc54813375bbee0da4af172288ebfc6
Peter Wallhead <peter.wallhead@gmail.com>
2025-05-23 10:55:12 +1000

    Adds launch file to run the micro_ros_agent package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'slambot_odometry_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/slambot_odometry_pkg']),
+        ('share/slambot_odometry_pkg', ['package.xml']),
+        ('share/slambot_odometry_pkg/launch', ['launch/slambot_odometry.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='peter',
+    maintainer_email='peter.wallhead@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # Example for later:
+            # 'odometry_node = slambot_odometry_pkg.odometry_node:main',
+        ],
+    },
+)
